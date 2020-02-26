5d3468820f2f43d3079a3c9cb5a73651adef5ce7
Magnus Wallin <maulinglawns@users.noreply.github.com>
2020-02-26 14:11:45 +0100

    Use PID as argument

--- pstime/src/main.rs
+++ pstime/src/main.rs
@@ -1,12 +1,19 @@
 use std::process::Command;
+use std::env;
 
 fn main() {
+    // Get argument(s)
+    let args: Vec<String> = env::args().collect();
+    // Run command
     let ps_out = Command::new("ps")
                         .arg("-o")
                         // Suppress header
                         .arg("pid=,user=,etime=,cmd=")
+                        // Supply PID below
+                        .arg(&args[1])
                         .output()
                         .expect("Failed to execute ps");
 
     println!("{}", String::from_utf8_lossy(&ps_out.stdout));
+    println!("{}", args[1]);
 }
