6e5086ce72554e5e60275048272cbf68413e64ce
Magnus Wallin <magnuswallin@tutanota.com>
2020-03-05 18:10:08 +0100

    Test arg that fails. FIX!

--- pstime/src/main.rs
+++ pstime/src/main.rs
@@ -1,19 +1,33 @@
-use std::process::Command;
+use std::process::{Command,exit};
 use std::env;
 
 fn main() {
+    let pid: i32;
+
     // Get argument(s)
     let args: Vec<String> = env::args().collect();
+
+    // Save PID here
+    if args[1].parse::<i32>().is_ok() {
+        //pid = args[1].parse::<i32>().unwrap();
+        pid = args[1];
+        println!("{}", pid);
+    } else {
+        println!("\"{}\" is not an integer.", args[1]);
+        exit(1);
+    }
+
     // Run command
     let ps_out = Command::new("ps")
                         .arg("-o")
                         // Suppress header
-                        .arg("pid=,user=,etime=,cmd=")
+                        //.arg("pid=,user=,etime=,cmd=")
+                        .arg("etime=")
                         // Supply PID below
-                        .arg(&args[1])
+                        //.arg(pid)
                         .output()
                         .expect("Failed to execute ps");
 
     println!("{}", String::from_utf8_lossy(&ps_out.stdout));
-    println!("{}", args[1]);
+    //println!("{}", args[1]);
 }
