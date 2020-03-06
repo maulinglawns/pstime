de58f4ba36efb663573ec0540bd9b0a19e9d6650
Magnus Wallin <maulinglawns@users.noreply.github.com>
2020-03-06 13:10:33 +0100

    Fix borrow bug
    Cleaner sanity check. No need for if/else

--- pstime/src/main.rs
+++ pstime/src/main.rs
@@ -2,29 +2,20 @@ use std::process::{Command,exit};
 use std::env;
 
 fn main() {
-    let pid: i32;
-
     // Get argument(s)
     let args: Vec<String> = env::args().collect();
 
-    // Save PID here
-    if args[1].parse::<i32>().is_ok() {
-        //pid = args[1].parse::<i32>().unwrap();
-        pid = args[1];
-        println!("{}", pid);
-    } else {
+    // Sanity check argument here, should be integers only
+    if args[1].parse::<i32>().is_err() {
         println!("\"{}\" is not an integer.", args[1]);
         exit(1);
-    }
+    }   
+
+    let pid = &args[1];
 
     // Run command
     let ps_out = Command::new("ps")
-                        .arg("-o")
-                        // Suppress header
-                        //.arg("pid=,user=,etime=,cmd=")
-                        .arg("etime=")
-                        // Supply PID below
-                        //.arg(pid)
+                        .args(&["-o", "etime=", pid])
                         .output()
                         .expect("Failed to execute ps");
 
